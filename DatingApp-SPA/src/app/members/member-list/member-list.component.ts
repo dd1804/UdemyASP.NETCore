a8fba996e612754722485a6160b43d4a906dff5d
Dorin <dorin.damaschin@gmail.com>
2018-09-13 00:01:20 +0300

    Added members ui

---
+++ DatingApp-SPA/src/app/members/member-list/member-list.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../_services/user.service';
+import { AlertityService } from '../../_services/alertity.service';
+import { User } from '../../_models/user';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-member-list',
+  templateUrl: './member-list.component.html',
+  styleUrls: ['./member-list.component.css']
+})
+export class MemberListComponent implements OnInit {
+  users: User[];
+
+  constructor(private userService: UserService, private alertityService: AlertityService, private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.route.data.subscribe(data => {
+      this.users = data['users'];
+    });
+  }
+
+}
