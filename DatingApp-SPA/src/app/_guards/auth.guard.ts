3dc1d4e9746d71fcab00e6e4bc4ed9b1d0fddb86
Dorin <dorin.damaschin@gmail.com>
2018-11-10 19:11:04 +0200

    Final main version

--- DatingApp-SPA/src/app/_guards/auth.guard.ts
+++ DatingApp-SPA/src/app/_guards/auth.guard.ts
@@ -15,7 +15,7 @@ export class AuthGuard implements CanActivate {
     if (this.authService.loggedIn()) {
       return true;
     }
-    
+
     this.alertifyService.error('You shall not pass!!!');
     this.router.navigate(['/home']);
     return false;
